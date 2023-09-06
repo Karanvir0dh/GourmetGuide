832893e6b609bc96ea5cd584cec87dbabc683a6a
Karanvir0dh <karanvird2@gmail.com>
2023-09-05 20:17:27 -0400

    error handling

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const path = require("path");
 const Restaurant = require("./models/restaurant");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const morgan = require("morgan");
+const catchAsync = require("./utils/CatchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
+const { restaurantSchema } = require("./schema");
+// const morgan = require("morgan");
+// const { error } = require("console");
 const app = express();
 
 mongoose
@@ -25,53 +30,96 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 // app.use(morgan("tiny"));
 
+const validateRestaurant = (req, res, next) => {
+  const { error } = restaurantSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.find({});
-  res.render("restaurants/index", { restaurants });
-});
+app.get(
+  "/restaurants",
+  catchAsync(async (req, res) => {
+    const restaurants = await Restaurant.find({});
+    res.render("restaurants/index", { restaurants });
+  })
+);
 
 app.get("/restaurants/new", async (req, res) => {
   res.render("restaurants/new");
 });
 
-app.post("/restaurants", async (req, res) => {
-  const restaurant = new Restaurant(req.body.restaurant);
-  await restaurant.save();
-  res.redirect(`/restaurants/${restaurant._id}`);
+app.post(
+  "/restaurants",
+  validateRestaurant,
+  catchAsync(async (req, res) => {
+    // if (!req.body.restaurant) throw new ExpressError("Invalid Restaurant Data", 400);
+    const restaurant = new Restaurant(req.body.restaurant);
+    await restaurant.save();
+    res.redirect(`/restaurants/${restaurant._id}`);
+  })
+);
+
+app.get(
+  "/restaurants/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const restaurant = await Restaurant.findById(id);
+    res.render("restaurants/show", { restaurant });
+  })
+);
+
+app.get(
+  "/restaurants/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const restaurant = await Restaurant.findById(id);
+    res.render("restaurants/edit", { restaurant });
+  })
+);
+
+app.put(
+  "/restaurants/:id",
+  validateRestaurant,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const restaurant = await Restaurant.findByIdAndUpdate(
+      id,
+      {
+        ...req.body.restaurant,
+      },
+      { new: true }
+    );
+    res.redirect(`/restaurants/${id}`);
+  })
+);
+
+app.delete(
+  "/restaurants/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Restaurant.findByIdAndDelete(id);
+    res.redirect("/restaurants");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
-app.get("/restaurants/:id", async (req, res) => {
-  const { id } = req.params;
-  const restaurant = await Restaurant.findById(id);
-  res.render("restaurants/show", { restaurant });
-});
-
-app.get("/restaurants/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const restaurant = await Restaurant.findById(id);
-  res.render("restaurants/edit", { restaurant });
-});
-
-app.put("/restaurants/:id", async (req, res) => {
-  const { id } = req.params;
-  const restaurant = await Restaurant.findByIdAndUpdate(
-    id,
-    {
-      ...req.body.restaurant,
-    },
-    { new: true }
-  );
-  res.redirect(`/restaurants/${id}`);
-});
-
-app.delete("/restaurants/:id", async (req, res) => {
-  const { id } = req.params;
-  await Restaurant.findByIdAndDelete(id);
-  res.redirect("/restaurants");
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.msg) {
+    err.msg = "Something went Wrong";
+  }
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
